d3b39b24852e800898dbba90fa3a51a508272622
L-Karimi <luciahkei101@gmail.com>
2022-05-24 16:26:42 +0300

    calculations

---
+++ calculate.py
@@ -0,0 +1,25 @@
+a=7
+b=2
+
+print(a**b)
+print(a%b)
+print(a//b)
+# My electricity bills for the last three months have been $23, $32 and $64.
+# What is the average monthly electricity bill over the three month period?
+# Write an expression to calculate 
+e1,e2,e3 = 23,32,64
+average = (e1+e2+e3)/3
+print(average)
+
+# Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+#How many tiles are needed?
+#You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+floor1=9*7
+floor2=5*7
+tiles = floor1+floor2
+print(tiles)
+total_tiles = (5*7) + (5*7)
+bought_tiles = (17*6)
+remaining_tiles = 102-98
+# Fill this in with an expression that calculates how many tiles will be left over.
+print(remaining_tiles)

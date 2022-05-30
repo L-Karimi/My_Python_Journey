b8576dad21b9e68497da2560746dfb07d2ad0602
L-Karimi <luciahkei101@gmail.com>
2022-05-30 14:30:53 +0300

    added strings file

--- strings.py
+++ strings.py
@@ -1,3 +1,6 @@
+from tracemalloc import start
+
+
 ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
 print(ford_quote)
 ford_quote = "Whether you think you can, or you think you can't--you're right."
@@ -55,3 +58,29 @@ print("Does your {} {}?".format(animal,action))
 
 maria_string = "Maria loves {} and {}"
 print(maria_string.format("English", "statistics"))
+
+family ="Karimis"
+family_members = 5
+
+print("The {} is made up of {} members".format(family,family_members))
+
+
+new_string = "The cow jumped over the wall to protect itself from the lion."
+print(new_string.split())
+print(new_string.split('.'))
+print(new_string.split(',',3))
+print(new_string.split(None,3))
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse)
+print(len(verse))
+print(verse.find("and"))
+print(verse.find("you"))
+print(verse.find("and"))
+print(verse.find("you",))
+print(verse.format('',3))
+print(verse, "\n")
+
+print("Verse has a length of {} characters.".format(len(verse)))
+print("The first occurence of the word 'and' occurs at the {}th index.".format(verse.find('and')))
+print("The last occurence of the word 'you' occurs at the {}th index.".format(verse.rfind('you')))
+print("The word 'you' occurs {} times in the verse.".format(verse.count('you')))

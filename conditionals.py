b1e2cb5c96ffc2bc23826d3f16f2ec7469fafd0a
L-Karimi <luciahkei101@gmail.com>
2022-06-07 07:48:16 +0300

    modified conditionals

--- conditionals.py
+++ conditionals.py
@@ -52,6 +52,12 @@ if number %2==0:
 else:
     print("Number"+str(number)+ " is odd")
     
+free_up_to_age = 4
+child_up_to_age = 18
+senior_from_age = 65
+age =67
+concession_ticket = 1.25
+adult_ticket = 2.50
 if age <= free_up_to_age:
     ticket_price = 0
 elif age <= child_up_to_age:
@@ -63,3 +69,37 @@ else:
 
 message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
 print(message)
+
+points = 174
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
+# '''
+# You decide you want to play a game where you are hiding 
+# a number from someone.  Store this number in a variable 
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 100
+guess = 22
+
+if guess<answer:
+    result = "Oops!  Your guess was too low."
+elif guess>answer:
+    result = "Oops!  Your guess was too high."
+elif answer==guess:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)

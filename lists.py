e5006df6555a665638ed8bb2c2273eaaf576d379
L-Karimi <luciahkei101@gmail.com>
2022-06-20 11:27:38 +0300

    added sets and list files

---
+++ lists.py
@@ -0,0 +1,15 @@
+a = [1,2,3,4,5,6,7,8,90]
+for num in a:
+    if 20 in a:
+        print("Number is available")
+    else:
+        print("Number is not available")
+        
+        
+        
+        
+def population_density(population,landsacape):
+    text1= population_density(100,400)
+    
+    return population
+print("expected results:")
---
+++ sets.py
@@ -0,0 +1,11 @@
+set1={"Java","Kotlin","Javascript"}
+set2={"Html","css","React","Nodejs"}
+new_set= set1.symmetric_difference(set2)
+print(new_set)
+
+set1={"Java","Kotlin","Javascript"}
+set2={"Html","css","React","Nodejs"}
+#print(new)
+numbers = [1,23,4,45,5,6,6,]
+my_sum=sum(numbers)
+print(my_sum)

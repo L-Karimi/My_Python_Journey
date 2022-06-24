5680c568678fe359e75f4a3c9347bfacd6982ccd
L-Karimi <luciahkei101@gmail.com>
2022-06-24 18:09:55 +0300

    two pointer functions

--- two_pointer_fun.py
+++ two_pointer_fun.py
@@ -8,3 +8,18 @@ def reverseArray(array):
 array = [10, 20, 30, 40, 50]      
 reverseArray(array)
 print(array)
+# write a program that reverses a string and leaving it still sensible without using inbuilt methods
+
+sentence="I am AkiraChix"
+
+def reverse_string(sentence):
+    new_sentence= sentence.split()
+    start=0
+    end=len(new_sentence)-1
+    while start<=end:
+        new_sentence[start],new_sentence[end]= new_sentence[end],new_sentence[start]
+        start +=1
+        end-=1
+        new_string=" ".join(new_sentence)
+        print(new_string)
+reverse_string("I am AkiraChix")
